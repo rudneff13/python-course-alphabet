d9c4615bfb0f897a0e6d3cb7f130c1915a595c05
Dmytro Rudniev <rudneff13@gmail.com>
2019-04-16 08:18:11 +0300

    Just one more try to handle homework

--- build_in_methods_iterators/work123.py
+++ build_in_methods_iterators/work123.py
@@ -1,7 +1,24 @@
+# def task_10_generator_of_simple_numbers() -> Generator[int, None, None]:
+#     """
+#     Return generator of simple numbers
+#     Stop then iteration if returned value is more than 200
+#     Examples:
+#         a = task_10_generator_of_simple_numbers()
+#         next(a)
+#         >>> 2
+#         next(a)
+#         >>> 3
+#     """
+#     pass
 
-data = "a A"
-summa = 0
-for piece in list(data):
-    summa += ord(piece)
+a = list(range(10))
+print(a)
 
-print(summa)
+new_list = []
+i = 1
+for i in a:
+
+    if a[i] % i == 0:
+        new_list.append(i)
+        i += 1
+print(new_list)
