aa8a47b6107d48af8d877f80f09c3c42f96874ed
rudneff13 <49341607+rudneff13@users.noreply.github.com>
2019-04-26 21:41:30 +0300

    Add files via upload

--- base_types_exceptions_loops/homework.py
+++ base_types_exceptions_loops/homework.py
@@ -15,7 +15,10 @@ def is_two_object_has_same_value(first: Any, second: Any) -> bool:
     If @first and @second has same value should return True
     In another case should return False
     """
-    pass
+    if first == second:
+        return True
+    else:
+        return False
 
 
 def is_two_objects_has_same_type(first: Any, second: Any) -> bool:
@@ -23,7 +26,10 @@ def is_two_objects_has_same_type(first: Any, second: Any) -> bool:
     If @first and @second has same type should return True
     In another case should return False
     """
-    pass
+    if type(first) == type(second):
+        return True
+    else:
+        return False
 
 
 def is_two_objects_is_the_same_objects(first: Any, second: Any) -> bool:
@@ -31,7 +37,10 @@ def is_two_objects_is_the_same_objects(first: Any, second: Any) -> bool:
     If @first and @second has same type should return True
     In another case should return False
     """
-    pass
+    if first is second:
+        return True
+    else:
+        return False
 
 
 def multiple_ints(first_value: int, second_value: int) -> int:
@@ -48,20 +57,23 @@ def multiple_ints(first_value: int, second_value: int) -> int:
     Returns:
         Product of elements
     """
-    pass
+    if isinstance(first_value, int) and isinstance(second_value, int):
+        return first_value * second_value
+    else:
+        raise ValueError
 
 
 def multiple_ints_with_conversion(first_value: Any, second_value: Any) -> int:
     """
     If possible to convert arguments to int value - convert and multiply them.
-    If it is impossible raise ValueError
+    If it is impossible raise OurAwesomeException
 
     Args:
         first_value: number for multiply
         second_value: number for multiply
 
     Raises:
-        ValueError
+        OurAwesomeException
 
     Returns: multiple of two numbers.
 
@@ -78,7 +90,14 @@ def multiple_ints_with_conversion(first_value: Any, second_value: Any) -> int:
             print("Not valid input data")
         >>> "Not valid input data"
     """
-    pass
+    # if type(int(first_value)) == type(int(second_value)) == int:
+    #         return int(first_value) * int(second_value)
+    # else:
+    #     raise OurAwesomeException
+    if isinstance(int(first_value), int) and isinstance(int(second_value), int):
+        return int(first_value) * int(second_value)
+    else:
+        raise OurAwesomeException
 
 
 def is_word_in_text(word: str, text: str) -> bool:
@@ -97,14 +116,25 @@ def is_word_in_text(word: str, text: str) -> bool:
         >>> False
 
     """
-    pass
+    if word in text:
+        return True
+    else:
+        return False
 
 
 def some_loop_exercise() -> list:
     """
     Use loop to create list that contain int values from 0 to 12 except 6 and 7
     """
-    pass
+    new_list = []
+    i = 0
+    while i < 13:
+        if i == 6 or i == 7:
+            pass
+        else:
+            new_list.append(i)
+        i += 1
+    return new_list
 
 
 def remove_from_list_all_negative_numbers(data: List[int]) -> list:
@@ -116,7 +146,30 @@ def remove_from_list_all_negative_numbers(data: List[int]) -> list:
         remove_from_list_all_negative_numbers([1, 5, -7, 8, -1])
         >>> [1, 5, 8]
     """
-    pass
+    # new_list = []
+    # for value in data:
+    #     if value >=0:
+    #         new_list.append(value)
+    #     else:
+    #         pass
+    # return new_list
+
+    i = 0
+    while i < len(data):
+        if data[i] >= 0:
+            i += 1
+        else:
+            data.remove(data[i])
+    return data
+
+    # j = 0
+    # for i in range(0, len(List)):
+    #     if List[j] >= 0:
+    #         j += 1
+    #         continue
+    #     else:
+    #         List.remove(List[j])
+    # return List
 
 
 def alphabet() -> dict:
@@ -127,7 +180,13 @@ def alphabet() -> dict:
         alphabet()
         >>> {"a": 1, "b": 2 ...}
     """
-    pass
+
+    import string
+
+    values = list(string.ascii_lowercase)
+    keys = list(range(1, len(values)+1))
+    new_dict = dict(zip(keys, values))
+    return new_dict
 
 
 def simple_sort(data: List[int]) -> List[list]:
@@ -139,4 +198,22 @@ def simple_sort(data: List[int]) -> List[list]:
     Returns:
 
     """
-    pass
+
+    # new_data = []
+    # for i in range(0, len(data)):
+    #     new_data.append(min(data))
+    #     data.remove(min(data))
+    # return new_data
+
+    # last_item = len(data)-1
+    # for i in range(0, last_item):
+    #     for j in range(0, last_item - i):
+    #         if data[j] > data[j + 1]:
+    #             data[j], data[j + 1] = data[j + 1], data[j]
+    # return data
+
+    for j in range(len(data) - 1, 0, -1):
+        for i in range(j):
+            if data[i] > data[i + 1]:
+                data[i], data[i + 1] = data[i + 1], data[i]
+    return data
