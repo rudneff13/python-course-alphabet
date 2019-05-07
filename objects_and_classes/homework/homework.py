5b25bed0e5b679b992231447f0b1f33752f39683
Dmytro Rudniev <rudneff13@gmail.com>
2019-05-07 11:43:57 +0300

    fixed homework4 by request of support

--- objects_and_classes/homework/homework.py
+++ objects_and_classes/homework/homework.py
@@ -47,27 +47,17 @@
     Колекціонерів можна порівнювати за ціною всіх їх автомобілів.
 """
 
-import constants as c
+from objects_and_classes.homework import constants as c
 import uuid
 from functools import reduce
+import json
 
 
 class Cesar:
-    def __init__(self, name, garages=0):
+    def __init__(self, name, garages=0, register_id=None):
         self.name = str(name)
         self.garages = garages
-        self.register_id = uuid.uuid4()
-
-    def __str__(self):
-        return f"""
-        Attributes of current Cesar:
-            name: '{self.name}';
-            total garages: '{len(self.garages)}';
-            register_id: '{self.register_id}';
-        """
-
-    def __repr__(self):
-        return f"""Cesar(name='{self.name}', garages='{len(self.garages)}')"""
+        self.register_id = register_id if register_id else uuid.uuid4()
 
     def hit_hat(self):
         return sum([sum([car.price for car in garage.cars]) for garage in self.garages])
@@ -93,6 +83,30 @@ class Cesar:
             else:
                 print("There's no free space at this garage for your car!")
 
+    def obj_to_dict(self):
+        obj_dict = {
+            "__class__": self.__class__.__name__,
+            "__module__": self.__module__
+        }
+        obj_dict.update(self.__dict__)
+
+        garages_serialized = []
+        for garage in self.garages:
+            garages_serialized.append(garage.obj_to_dict())
+        obj_dict.update({'garages': garages_serialized})
+        return obj_dict
+
+    def __str__(self):
+        return f"""
+        Attributes of current Cesar:
+            name: '{self.name}';
+            total garages: '{len(self.garages)}';
+            register_id: '{self.register_id}';
+        """
+
+    def __repr__(self):
+        return f"""Cesar(name='{self.name}', garages='{len(self.garages)}')"""
+
     def __gt__(self, other):
         return self.hit_hat() > other.hit_hat()
 
@@ -113,15 +127,30 @@ class Cesar:
 
 
 class Car:
-    def __init__(self, price, type, producer, mileage):
+    def __init__(self, price, type, producer, mileage, number=None):
         if type in c.CARS_TYPES and producer in c.CARS_PRODUCER:
             self.price = float(price)
             self.type = type
             self.producer = producer
-            self.number = uuid.uuid4()
+            self.number = number if number else uuid.uuid4()
             self.mileage = float(mileage)
         else:
             print('Unknown car type or producer')
+
+    def change_number(self, new_number):
+        try:
+            uuid.UUID(new_number, version=4)
+            self.number = new_number
+        except (ValueError, AttributeError, TypeError):
+            print("It's not UUID number!")
+
+    def obj_to_dict(self):
+        obj_dict = {
+            "__class__": self.__class__.__name__,
+            "__module__": self.__module__
+        }
+        obj_dict.update(self.__dict__)
+        return obj_dict
 
     def __str__(self):
         return f"""
@@ -154,16 +183,8 @@ class Car:
     def __ne__(self, other):
         return self.price != other.price
 
-    def change_number(self, new_number):
-        try:
-            uuid.UUID(new_number, version=4)
-            self.number = new_number
-        except ValueError or AttributeError or TypeError:
-            print("It's not UUID number!")
-
 
 class Garage:
-
     def __init__(self, town, cars, places, owner=None):
         if town in c.TOWNS:
             self.town = town
@@ -172,18 +193,6 @@ class Garage:
             self.owner = owner
         else:
             print('Unknown town name')
-
-    def __str__(self):
-        return f"""
-        Attributes of current Garage:
-            town: '{self.town}';
-            total cars: '{len(self.cars)}';
-            places: '{self.places}';
-            owner: '{self.owner}';
-        """
-
-    def __repr__(self):
-        return f"""Garage(town='{self.town}', cars='{len(self.cars)}', places='{self.places}', owner='{self.owner}')"""
 
     def add_car(self, new_car):
         free_places = self.places - len(self.cars)
@@ -210,14 +219,61 @@ class Garage:
     def free_places(self):
         return self.places - len(self.cars)
 
+    def obj_to_dict(self):
+        obj_dict = {
+            "__class__": self.__class__.__name__,
+            "__module__": self.__module__
+        }
+        obj_dict.update(self.__dict__)
+
+        cars_serialized = []
+        for car in self.cars:
+            cars_serialized.append(car.obj_to_dict())
+        obj_dict.update({'cars': cars_serialized})
+        return obj_dict
+
+    def __str__(self):
+        return f"""
+        Attributes of current Garage:
+            town: '{self.town}';
+            total cars: '{len(self.cars)}';
+            places: '{self.places}';
+            owner: '{self.owner}';
+        """
+
+    def __repr__(self):
+        return f"""Garage(town='{self.town}', cars='{len(self.cars)}', places='{self.places}', owner='{self.owner}')"""
+
+
+class JsonEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, uuid.UUID):
+            return obj.hex
+        return json.JSONEncoder.default(self, obj)
+
+
+def dict_to_obj(our_dict):
+    if 'number' or 'register_id' in our_dict:
+        our_dict.update({key: uuid.UUID(value) for key, value in our_dict.items()
+                         if key=='number' or key=='register_id'})
+    if "__class__" in our_dict:
+        class_name = our_dict.pop("__class__")
+        module_name = our_dict.pop("__module__")
+        module = __import__(module_name)
+        class_ = getattr(module, class_name)
+        obj = class_(**our_dict)
+    else:
+        obj = our_dict
+    return obj
+
 
 if __name__ == "__main__":
 
     car1 = Car(1001, "Diesel", "BMW",  1123)
     car2 = Car(2000, "Crossover", "BENTLEY", 5422)
-    garage1 = Garage('Kiev', [car1, car2], 2, "Lolka_owner")
-    garage2 = Garage('Prague', [car2], 12, "Lolka_owner")
-    garage3 = Garage('Prague', [car2, car1], 10, "Lolka_owner")
+    garage1 = Garage('Kiev', [car1, car2], 2, )
+    garage2 = Garage('Prague', [car2], 12, )
+    garage3 = Garage('Prague', [car2, car1], 10, )
     cesar1 = Cesar("Crappy", [garage1, garage2, garage3])
     cesar2 = Cesar("Another guy", [garage1])
 
